bcb39f44bb24ba76f589f77829031558fda86534
Kévin Chalet <kevinchalet@gmail.com>
2015-10-04 18:41:38 +0200

    Add license headers

--- src/OpenIddict.Models/ApplicationType.cs
+++ src/OpenIddict.Models/ApplicationType.cs
@@ -1,4 +1,10 @@
-﻿namespace OpenIddict.Models {
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/openiddict/core for more information concerning
+ * the license and the contributors participating to this project.
+ */
+
+namespace OpenIddict.Models {
     public enum ApplicationType {
         Public = 0,
         Confidential = 1
