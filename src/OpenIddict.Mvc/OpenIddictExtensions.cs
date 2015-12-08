4fdf9b4ca78bb2555a22e4682afb409182198f7e
David McCullough <damccull@gmail.com>
2015-12-07 19:57:24 -0600

    Replace NWebsec.Owin with NWebsec.Middleware which supports dnxcore50.

--- src/OpenIddict.Mvc/OpenIddictExtensions.cs
+++ src/OpenIddict.Mvc/OpenIddictExtensions.cs
@@ -16,58 +16,60 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Internal;
 using Microsoft.Extensions.Primitives;
 using OpenIddict;
-
-#if DNX451
-using NWebsec.Owin;
-#endif
+using NWebsec.Middleware;
 
 namespace Microsoft.AspNet.Builder {
     public static class OpenIddictExtensions {
         public static OpenIddictBuilder UseMvc([NotNull] this OpenIddictBuilder builder) {
-#if DNX451
-            builder.AddModule(-20, app => app.UseKatana(owin => {
+
+            builder.AddModule(-20, app => {
                 // Insert a new middleware responsible of setting the Content-Security-Policy header.
                 // See https://nwebsec.codeplex.com/wikipage?title=Configuring%20Content%20Security%20Policy&referringTitle=NWebsec
-                owin.UseCsp(options => options.DefaultSources(directive => directive.Self())
-                                              .ImageSources(directive => directive.Self().CustomSources("*"))
-                                              .ScriptSources(directive => directive.Self().UnsafeInline())
-                                              .StyleSources(directive => directive.Self().UnsafeInline()));
+                app.UseCsp(options => {
+                    options.DefaultSources(directive => directive.Self())
+                        .ImageSources(directive => directive.Self().CustomSources("*"))
+                        .ScriptSources(directive => directive.Self().UnsafeInline())
+                        .StyleSources(directive => directive.Self().UnsafeInline());
+                });
 
                 // Insert a new middleware responsible of setting the X-Content-Type-Options header.
                 // See https://nwebsec.codeplex.com/wikipage?title=Configuring%20security%20headers&referringTitle=NWebsec
-                owin.UseXContentTypeOptions();
+                app.UseXContentTypeOptions();
 
                 // Insert a new middleware responsible of setting the X-Frame-Options header.
                 // See https://nwebsec.codeplex.com/wikipage?title=Configuring%20security%20headers&referringTitle=NWebsec
-                owin.UseXfo(options => options.Deny());
+                app.UseXfo(options=>options.Deny());
 
                 // Insert a new middleware responsible of setting the X-Xss-Protection header.
                 // See https://nwebsec.codeplex.com/wikipage?title=Configuring%20security%20headers&referringTitle=NWebsec
-                owin.UseXXssProtection(options => options.EnabledWithBlockMode());
-            }));
-#endif
+                app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            });
 
             // Run the rest of the pipeline in an isolated environment.
             builder.AddModule(10, app => app.Isolate(map => map.UseMvc(routes => {
                 // Register the actions corresponding to the authorization endpoint.
-                if (builder.Options.AuthorizationEndpointPath.HasValue) {
+                if(builder.Options.AuthorizationEndpointPath.HasValue) {
                     routes.MapRoute("{D97891B4}", builder.Options.AuthorizationEndpointPath.Value.Substring(1), new {
-                        controller = "OpenIddict", action = nameof(OpenIddictController<object, object>.Authorize)
+                        controller = "OpenIddict",
+                        action = nameof(OpenIddictController<object, object>.Authorize)
                     });
 
                     routes.MapRoute("{7148DB83}", builder.Options.AuthorizationEndpointPath.Value.Substring(1) + "/accept", new {
-                        controller = "OpenIddict", action = nameof(OpenIddictController<object, object>.Accept)
+                        controller = "OpenIddict",
+                        action = nameof(OpenIddictController<object, object>.Accept)
                     });
 
                     routes.MapRoute("{23438BCC}", builder.Options.AuthorizationEndpointPath.Value.Substring(1) + "/deny", new {
-                        controller = "OpenIddict", action = nameof(OpenIddictController<object, object>.Deny)
+                        controller = "OpenIddict",
+                        action = nameof(OpenIddictController<object, object>.Deny)
                     });
                 }
 
                 // Register the action corresponding to the logout endpoint.
-                if (builder.Options.LogoutEndpointPath.HasValue) {
+                if(builder.Options.LogoutEndpointPath.HasValue) {
                     routes.MapRoute("{C7DB102A}", builder.Options.LogoutEndpointPath.Value.Substring(1), new {
-                        controller = "OpenIddict", action = nameof(OpenIddictController<object, object>.Logout)
+                        controller = "OpenIddict",
+                        action = nameof(OpenIddictController<object, object>.Logout)
                     });
                 }
             }), services => {
@@ -97,7 +99,7 @@ namespace Microsoft.AspNet.Builder {
                 // Register the sign-in manager in the isolated container.
                 services.AddScoped(typeof(SignInManager<>).MakeGenericType(registration.UserType), provider => {
                     var accessor = provider.GetRequiredService<IHttpContextAccessor>();
-                    var container = (IServiceProvider) accessor.HttpContext.Items[typeof(IServiceProvider)];
+                    var container = (IServiceProvider)accessor.HttpContext.Items[typeof(IServiceProvider)];
                     Debug.Assert(container != null);
 
                     // Resolve the sign-in manager from the parent container.
@@ -107,7 +109,7 @@ namespace Microsoft.AspNet.Builder {
                 // Register the user manager in the isolated container.
                 services.AddScoped(typeof(OpenIddictManager<,>).MakeGenericType(registration.UserType, registration.ApplicationType), provider => {
                     var accessor = provider.GetRequiredService<IHttpContextAccessor>();
-                    var container = (IServiceProvider) accessor.HttpContext.Items[typeof(IServiceProvider)];
+                    var container = (IServiceProvider)accessor.HttpContext.Items[typeof(IServiceProvider)];
                     Debug.Assert(container != null);
 
                     // Resolve the user manager from the parent container.
@@ -142,11 +144,11 @@ namespace Microsoft.AspNet.Builder {
             public IList<IFileProvider> Providers { get; }
 
             public IDirectoryContents GetDirectoryContents(string subpath) {
-                for (var index = 0; index < Providers.Count; index++) {
+                for(var index = 0; index < Providers.Count; index++) {
                     var provider = Providers[index];
 
                     var result = provider.GetDirectoryContents(subpath);
-                    if (result != null && result.Exists) {
+                    if(result != null && result.Exists) {
                         return result;
                     }
                 }
@@ -155,11 +157,11 @@ namespace Microsoft.AspNet.Builder {
             }
 
             public IFileInfo GetFileInfo(string subpath) {
-                for (var index = 0; index < Providers.Count; index++) {
+                for(var index = 0; index < Providers.Count; index++) {
                     var provider = Providers[index];
 
                     var result = provider.GetFileInfo(subpath);
-                    if (result != null && result.Exists) {
+                    if(result != null && result.Exists) {
                         return result;
                     }
                 }
@@ -168,11 +170,11 @@ namespace Microsoft.AspNet.Builder {
             }
 
             public IChangeToken Watch(string filter) {
-                for (var index = 0; index < Providers.Count; index++) {
+                for(var index = 0; index < Providers.Count; index++) {
                     var provider = Providers[index];
 
                     var result = provider.Watch(filter);
-                    if (result != null) {
+                    if(result != null) {
                         return result;
                     }
                 }
