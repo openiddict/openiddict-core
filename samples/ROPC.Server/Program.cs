14b064b57ab8e8eda74123f4f56cd897b146a656
Shaun Luttin <admin@shaunluttin.com>
2016-04-26 17:17:06 -0700

    Use InMemory database.

--- samples/ROPC.Server/Program.cs
+++ samples/ROPC.Server/Program.cs
@@ -1,12 +1,68 @@
-﻿using System;
-
-namespace ConsoleApplication
+﻿namespace Application
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Infrastructure;
+    using Application.Database;
+
     public class Program
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddEntityFramework()
+                .AddEntityFrameworkInMemoryDatabase()
+                .AddDbContext<MyDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase();
+                });
+        }
+
+        public void Configure(IApplicationBuilder builder, MyDbContext dbContext)
+        {
+            // dbContext.Sandboxes.Add(new Sandbox());
+            // dbContext.SaveChangesAsync();
+            
+            builder.Run(context =>
+            {
+                return context.Response.WriteAsync("Hello.");
+            });
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Program>()
+                .Build();
+
+            builder.Run();
         }
+    }
+
+}
+
+namespace Application.Database
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Sandbox> Sandboxes { get; set; }
+
+        public MyDbContext()
+        { }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+    }
+
+    public class Sandbox
+    {
+        public int SandboxId { get; set; }
     }
 }
