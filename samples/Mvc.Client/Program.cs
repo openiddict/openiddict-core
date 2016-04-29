b163497c7ecdebc3976fbe93f03e60549f9c5aa2
Kévin Chalet <kevinchalet@gmail.com>
2016-04-29 06:48:42 +0200

    React to API changes in aspnet/Hosting
    https://github.com/aspnet/Hosting/commit/e505ecbc2176cc35b6fb0e3fd51fc7bf4c484cf4

--- samples/Mvc.Client/Program.cs
+++ samples/Mvc.Client/Program.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Mvc.Client {
     public static class Program {
         public static void Main(string[] args) {
+            var configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
             var host = new WebHostBuilder()
                 .ConfigureLogging(options => options.AddConsole())
                 .ConfigureLogging(options => options.AddDebug())
-                .UseDefaultHostingConfiguration(args)
+                .UseConfiguration(configuration)
                 .UseIISIntegration()
                 .UseKestrel()
                 .UseStartup<Startup>()
