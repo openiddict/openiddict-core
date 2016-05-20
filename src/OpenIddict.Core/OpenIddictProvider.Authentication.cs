ec1ef64a8fd010815cf9bf6356e35e989acb13de
Massimiliano Donini <massimiliano.donini@gmail.com>
2016-05-21 00:38:17 +0200

    Added logging to ValidateAuthorizationRequest

--- src/OpenIddict.Core/OpenIddictProvider.Authentication.cs
+++ src/OpenIddict.Core/OpenIddictProvider.Authentication.cs
@@ -6,7 +6,6 @@
 
 using System;
 using System.Diagnostics;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -16,17 +15,21 @@ using JetBrains.Annotations;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-namespace OpenIddict {
+namespace OpenIddict
+{
     public partial class OpenIddictProvider<TUser, TApplication> : OpenIdConnectServerProvider where TUser : class where TApplication : class {
         public override async Task ValidateAuthorizationRequest([NotNull] ValidateAuthorizationRequestContext context) {
             var services = context.HttpContext.RequestServices.GetRequiredService<OpenIddictServices<TUser, TApplication>>();
+            var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<OpenIddictProvider<TUser, TApplication>>>();
 
             // Note: redirect_uri is not required for pure OAuth2 requests
             // but this provider uses a stricter policy making it mandatory,
             // as required by the OpenID Connect core specification.
             // See http://openid.net/specs/openid-connect-core-1_0.html#AuthRequest.
             if (string.IsNullOrEmpty(context.RedirectUri)) {
+                logger.LogWarning("The required redirect_uri parameter was missing.");
                 context.Reject(
                     error: OpenIdConnectConstants.Errors.InvalidRequest,
                     description: "The required redirect_uri parameter was missing.");
@@ -37,6 +40,7 @@ namespace OpenIddict {
             // Retrieve the application details corresponding to the requested client_id.
             var application = await services.Applications.FindApplicationByIdAsync(context.ClientId);
             if (application == null) {
+                logger.LogWarning($"Application not found in the database with client_id '{context.ClientId}'.");
                 context.Reject(
                     error: OpenIdConnectConstants.Errors.InvalidClient,
                     description: "Application not found in the database: ensure that your client_id is correct.");
@@ -45,6 +49,7 @@ namespace OpenIddict {
             }
 
             if (!await services.Applications.ValidateRedirectUriAsync(application, context.RedirectUri)) {
+                logger.LogWarning($"Validation for redirect_uri {context.RedirectUri} failed.");
                 context.Reject(
                     error: OpenIdConnectConstants.Errors.InvalidClient,
                     description: "Invalid redirect_uri.");
@@ -57,6 +62,7 @@ namespace OpenIddict {
             // Note: when using the authorization code grant, ValidateClientAuthentication is responsible of
             // rejecting the token request if the client_id corresponds to an unauthenticated confidential client.
             if (await services.Applications.IsConfidentialApplicationAsync(application) && !context.Request.IsAuthorizationCodeFlow()) {
+                logger.LogWarning($"Confidential clients cannot use response_type '{context.Request.ResponseType}'.");
                 context.Reject(
                     error: OpenIdConnectConstants.Errors.InvalidRequest,
                     description: "Confidential clients can only use response_type=code.");
@@ -70,6 +76,7 @@ namespace OpenIddict {
                 // Ensure the user profile still exists in the database.
                 var user = await services.Users.GetUserAsync(context.HttpContext.User);
                 if (user == null) {
+                    logger.LogWarning("Unable to retrieve the logged user from the store.");
                     context.Reject(
                         error: OpenIdConnectConstants.Errors.ServerError,
                         description: "An internal error has occurred.");
@@ -86,6 +93,7 @@ namespace OpenIddict {
                     var email = await services.Users.GetEmailAsync(user);
 
                     if (!string.IsNullOrEmpty(email) && string.Equals(username, email, StringComparison.OrdinalIgnoreCase)) {
+                        logger.LogWarning("The username correspond to the email address and we carefully avoid leaking the user email if the 'email' scope is not requested.");
                         context.Reject(
                             error: OpenIdConnectConstants.Errors.InvalidRequest,
                             description: "The 'email' scope is required.");
@@ -100,6 +108,7 @@ namespace OpenIddict {
                 // If the user is not authenticated, return an error to the client application.
                 // See http://openid.net/specs/openid-connect-core-1_0.html#Authenticates
                 if (!context.HttpContext.User.Identities.Any(identity => identity.IsAuthenticated)) {
+                    logger.LogWarning("Unable to silently authenticate the user and none already authenticated was found.");
                     context.Reject(
                         error: OpenIdConnectConstants.Errors.LoginRequired,
                         description: "The user must be authenticated.");
@@ -110,6 +119,7 @@ namespace OpenIddict {
                 // Ensure that the authentication cookie contains the required NameIdentifier claim.
                 var identifier = context.HttpContext.User.GetClaim(ClaimTypes.NameIdentifier);
                 if (string.IsNullOrEmpty(identifier)) {
+                    logger.LogWarning($"The authenticated user is invalid, it doesn't have a claim of type '{ClaimTypes.NameIdentifier}'.");
                     context.Reject(
                         error: OpenIdConnectConstants.Errors.ServerError,
                         description: "The authorization request cannot be processed.");
@@ -118,9 +128,10 @@ namespace OpenIddict {
                 }
 
                 // Extract the principal contained in the id_token_hint parameter.
-                // If no principal can be extracted, an error is returned to the client application.
+                // If no principal can be extracted, an error is returned to the client aplication.
                 var principal = await context.HttpContext.Authentication.AuthenticateAsync(context.Options.AuthenticationScheme);
                 if (principal == null) {
+                    logger.LogWarning("The required id_token_hint parameter is missing.");
                     context.Reject(
                         error: OpenIdConnectConstants.Errors.InvalidRequest,
                         description: "The required id_token_hint parameter is missing.");
@@ -132,6 +143,7 @@ namespace OpenIddict {
                 // and that the identity token corresponds to the authenticated user.
                 if (!principal.HasClaim(OpenIdConnectConstants.Claims.Audience, context.Request.ClientId) ||
                     !principal.HasClaim(ClaimTypes.NameIdentifier, identifier)) {
+                    logger.LogWarning("The id_token_hint parameter is invalid.");
                     context.Reject(
                         error: OpenIdConnectConstants.Errors.InvalidRequest,
                         description: "The id_token_hint parameter is invalid.");
@@ -140,6 +152,7 @@ namespace OpenIddict {
                 }
             }
 
+            logger.LogInformation("ValidateAuthorizationRequestContext is validated succesfully.");
             context.Validate();
         }
 
