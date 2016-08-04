70130ea82222e46f451341007fb3ebf3398e114b
Massimiliano Donini <massimiliano.donini@gmail.com>
2016-08-04 23:26:25 +0200

    Added few more test case

---
+++ tests/OpenIddict.Tests/BaseHostedTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using OpenIddict.Tests.Infrastructure;
+
+namespace OpenIddict.Tests
+{
+    public abstract class BaseHostedTests
+    {
+        protected async Task UseTestHost(Func<HttpClient, Task> invokeAction)
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .UseStartup<Startup>();
+
+            using (var host = new TestServer(webHostBuilder))
+            {
+                using (var client = host.CreateClient())
+                {
+                    await invokeAction.Invoke(client);
+                }
+            }
+        }
+    }
+}
