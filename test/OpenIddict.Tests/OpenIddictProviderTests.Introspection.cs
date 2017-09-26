df6bafb723d634f0985dc8ec66b12bf7e68278b3
Kévin Chalet <kevinchalet@gmail.com>
2017-09-26 22:52:27 +0200

    Add missing license headers

--- test/OpenIddict.Tests/OpenIddictProviderTests.Introspection.cs
+++ test/OpenIddict.Tests/OpenIddictProviderTests.Introspection.cs
@@ -1,4 +1,10 @@
-﻿using System.Security.Claims;
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/openiddict/openiddict-core for more information concerning
+ * the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
 using AspNet.Security.OpenIdConnect.Client;
