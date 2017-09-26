3c1757191904b58c869bad76a918e29913436ef4
Kévin Chalet <kevinchalet@gmail.com>
2017-09-26 22:51:35 +0200

    Introduce built-in Entity Framework 6.x stores for OpenIddict

---
+++ src/OpenIddict.EntityFramework/Stores/OpenIddictTokenStore.cs
@@ -0,0 +1,347 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/openiddict/openiddict-core for more information concerning
+ * the license and the contributors participating to this project.
+ */
+
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using OpenIddict.Core;
+using OpenIddict.Models;
+
+namespace OpenIddict.EntityFramework
+{
+    /// <summary>
+    /// Provides methods allowing to manage the tokens stored in a database.
+    /// Note: this class can only be used with the default OpenIddict entities.
+    /// </summary>
+    /// <typeparam name="TContext">The type of the Entity Framework database context.</typeparam>
+    public class OpenIddictTokenStore<TContext> : OpenIddictTokenStore<OpenIddictToken,
+                                                                       OpenIddictApplication,
+                                                                       OpenIddictAuthorization, TContext, string>
+        where TContext : DbContext
+    {
+        public OpenIddictTokenStore([NotNull] TContext context) : base(context) { }
+    }
+
+    /// <summary>
+    /// Provides methods allowing to manage the tokens stored in a database.
+    /// Note: this class can only be used with the default OpenIddict entities.
+    /// </summary>
+    /// <typeparam name="TContext">The type of the Entity Framework database context.</typeparam>
+    /// <typeparam name="TKey">The type of the entity primary keys.</typeparam>
+    public class OpenIddictTokenStore<TContext, TKey> : OpenIddictTokenStore<OpenIddictToken<TKey>,
+                                                                             OpenIddictApplication<TKey>,
+                                                                             OpenIddictAuthorization<TKey>, TContext, TKey>
+        where TContext : DbContext
+        where TKey : IEquatable<TKey>
+    {
+        public OpenIddictTokenStore([NotNull] TContext context) : base(context) { }
+    }
+
+    /// <summary>
+    /// Provides methods allowing to manage the tokens stored in a database.
+    /// Note: this class can only be used with the default OpenIddict entities.
+    /// </summary>
+    /// <typeparam name="TToken">The type of the Token entity.</typeparam>
+    /// <typeparam name="TApplication">The type of the Application entity.</typeparam>
+    /// <typeparam name="TAuthorization">The type of the Authorization entity.</typeparam>
+    /// <typeparam name="TContext">The type of the Entity Framework database context.</typeparam>
+    /// <typeparam name="TKey">The type of the entity primary keys.</typeparam>
+    public class OpenIddictTokenStore<TToken, TApplication, TAuthorization, TContext, TKey> :
+        OpenIddictTokenStore<TToken, TApplication, TAuthorization, TKey>
+        where TToken : OpenIddictToken<TKey, TApplication, TAuthorization>, new()
+        where TApplication : OpenIddictApplication<TKey, TAuthorization, TToken>, new()
+        where TAuthorization : OpenIddictAuthorization<TKey, TApplication, TToken>, new()
+        where TContext : DbContext
+        where TKey : IEquatable<TKey>
+    {
+        public OpenIddictTokenStore([NotNull] TContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            Context = context;
+        }
+
+        /// <summary>
+        /// Gets the database context associated with the current store.
+        /// </summary>
+        protected virtual TContext Context { get; }
+
+        /// <summary>
+        /// Gets the database set corresponding to the <typeparamref name="TApplication"/> entity.
+        /// </summary>
+        protected DbSet<TApplication> Applications => Context.Set<TApplication>();
+
+        /// <summary>
+        /// Gets the database set corresponding to the <typeparamref name="TAuthorization"/> entity.
+        /// </summary>
+        protected DbSet<TAuthorization> Authorizations => Context.Set<TAuthorization>();
+
+        /// <summary>
+        /// Gets the database set corresponding to the <typeparamref name="TToken"/> entity.
+        /// </summary>
+        protected DbSet<TToken> Tokens => Context.Set<TToken>();
+
+        /// <summary>
+        /// Creates a new token.
+        /// </summary>
+        /// <param name="token">The token to create.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that can be used to abort the operation.</param>
+        /// <returns>
+        /// A <see cref="Task"/> that can be used to monitor the asynchronous operation, whose result returns the token.
+        /// </returns>
+        public override async Task<TToken> CreateAsync([NotNull] TToken token, CancellationToken cancellationToken)
+        {
+            if (token == null)
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            Tokens.Add(token);
+
+            await Context.SaveChangesAsync(cancellationToken);
+
+            return token;
+        }
+
+        /// <summary>
+        /// Creates a new token, which is associated with a particular subject.
+        /// </summary>
+        /// <param name="descriptor">The token descriptor.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that can be used to abort the operation.</param>
+        /// <returns>
+        /// A <see cref="Task"/> that can be used to monitor the asynchronous operation, whose result returns the token.
+        /// </returns>
+        public override async Task<TToken> CreateAsync([NotNull] OpenIddictTokenDescriptor descriptor, CancellationToken cancellationToken)
+        {
+            if (descriptor == null)
+            {
+                throw new ArgumentNullException(nameof(descriptor));
+            }
+
+            var token = new TToken
+            {
+                Ciphertext = descriptor.Ciphertext,
+                CreationDate = descriptor.CreationDate,
+                ExpirationDate = descriptor.ExpirationDate,
+                Hash = descriptor.Hash,
+                Subject = descriptor.Subject,
+                Type = descriptor.Type
+            };
+
+            // Bind the token to the specified client application, if applicable.
+            if (!string.IsNullOrEmpty(descriptor.ApplicationId))
+            {
+                var key = ConvertIdentifierFromString(descriptor.ApplicationId);
+
+                var application = await Applications.SingleOrDefaultAsync(entity => entity.Id.Equals(key));
+                if (application == null)
+                {
+                    throw new InvalidOperationException("The application associated with the token cannot be found.");
+                }
+
+                token.Application = application;
+            }
+
+            // Bind the token to the specified authorization, if applicable.
+            if (!string.IsNullOrEmpty(descriptor.AuthorizationId))
+            {
+                var key = ConvertIdentifierFromString(descriptor.AuthorizationId);
+
+                var authorization = await Authorizations.SingleOrDefaultAsync(entity => entity.Id.Equals(key));
+                if (authorization == null)
+                {
+                    throw new InvalidOperationException("The authorization associated with the token cannot be found.");
+                }
+
+                token.Authorization = authorization;
+            }
+
+            return await CreateAsync(token, cancellationToken);
+        }
+
+        /// <summary>
+        /// Removes a token.
+        /// </summary>
+        /// <param name="token">The token to delete.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that can be used to abort the operation.</param>
+        /// <returns>A <see cref="Task"/> that can be used to monitor the asynchronous operation.</returns>
+        public override async Task DeleteAsync([NotNull] TToken token, CancellationToken cancellationToken)
+        {
+            if (token == null)
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            Tokens.Remove(token);
+
+            try
+            {
+                await Context.SaveChangesAsync(cancellationToken);
+            }
+
+            catch (DbUpdateConcurrencyException) { }
+        }
+
+        /// <summary>
+        /// Executes the specified query.
+        /// </summary>
+        /// <typeparam name="TResult">The result type.</typeparam>
+        /// <param name="query">The query to execute.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that can be used to abort the operation.</param>
+        /// <returns>
+        /// A <see cref="Task"/> that can be used to monitor the asynchronous operation,
+        /// whose result returns the single element returned when executing the specified query.
+        /// </returns>
+        public override Task<TResult> GetAsync<TResult>([NotNull] Func<IQueryable<TToken>, IQueryable<TResult>> query, CancellationToken cancellationToken)
+        {
+            if (query == null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
+            return query.Invoke(Tokens).SingleOrDefaultAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Executes the specified query.
+        /// </summary>
+        /// <typeparam name="TResult">The result type.</typeparam>
+        /// <param name="query">The query to execute.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that can be used to abort the operation.</param>
+        /// <returns>
+        /// A <see cref="Task"/> that can be used to monitor the asynchronous operation,
+        /// whose result returns all the elements returned when executing the specified query.
+        /// </returns>
+        public override Task<TResult[]> ListAsync<TResult>([NotNull] Func<IQueryable<TToken>, IQueryable<TResult>> query, CancellationToken cancellationToken)
+        {
+            if (query == null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
+            return query.Invoke(Tokens).ToArrayAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Sets the authorization associated with a token.
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <param name="identifier">The unique identifier associated with the authorization.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that can be used to abort the operation.</param>
+        /// <returns>
+        /// A <see cref="Task"/> that can be used to monitor the asynchronous operation.
+        /// </returns>
+        public override async Task SetAuthorizationAsync([NotNull] TToken token, [CanBeNull] string identifier, CancellationToken cancellationToken)
+        {
+            if (token == null)
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            if (!string.IsNullOrEmpty(identifier))
+            {
+                var key = ConvertIdentifierFromString(identifier);
+
+                var authorization = await Authorizations.SingleOrDefaultAsync(element => element.Id.Equals(key));
+                if (authorization == null)
+                {
+                    throw new InvalidOperationException("The authorization associated with the token cannot be found.");
+                }
+
+                authorization.Tokens.Add(token);
+            }
+
+            else
+            {
+                var key = await GetIdAsync(token, cancellationToken);
+
+                // Try to retrieve the authorization associated with the token.
+                // If none can be found, assume that no authorization is attached.
+                var authorization = await Authorizations.SingleOrDefaultAsync(element => element.Tokens.Any(t => t.Id.Equals(key)));
+                if (authorization != null)
+                {
+                    authorization.Tokens.Remove(token);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets the client application associated with a token.
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <param name="identifier">The unique identifier associated with the client application.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that can be used to abort the operation.</param>
+        /// <returns>
+        /// A <see cref="Task"/> that can be used to monitor the asynchronous operation.
+        /// </returns>
+        public override async Task SetClientAsync([NotNull] TToken token, [CanBeNull] string identifier, CancellationToken cancellationToken)
+        {
+            if (token == null)
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            if (!string.IsNullOrEmpty(identifier))
+            {
+                var key = ConvertIdentifierFromString(identifier);
+
+                var application = await Applications.SingleOrDefaultAsync(element => element.Id.Equals(key));
+                if (application == null)
+                {
+                    throw new InvalidOperationException("The application associated with the token cannot be found.");
+                }
+
+                application.Tokens.Add(token);
+            }
+
+            else
+            {
+                var key = await GetIdAsync(token, cancellationToken);
+
+                // Try to retrieve the application associated with the token.
+                // If none can be found, assume that no application is attached.
+                var application = await Applications.SingleOrDefaultAsync(element => element.Tokens.Any(t => t.Id.Equals(key)));
+                if (application != null)
+                {
+                    application.Tokens.Remove(token);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates an existing token.
+        /// </summary>
+        /// <param name="token">The token to update.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that can be used to abort the operation.</param>
+        /// <returns>
+        /// A <see cref="Task"/> that can be used to monitor the asynchronous operation.
+        /// </returns>
+        public override async Task UpdateAsync([NotNull] TToken token, CancellationToken cancellationToken)
+        {
+            if (token == null)
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            Tokens.Attach(token);
+            Context.Entry(token).State = EntityState.Modified;
+
+            try
+            {
+                await Context.SaveChangesAsync(cancellationToken);
+            }
+
+            catch (DbUpdateConcurrencyException) { }
+        }
+    }
+}
