78637a7d7714fa6b119bb109d5edf106ce1c6b01
Kévin Chalet <kevinchalet@gmail.com>
2019-12-09 21:36:53 +0100

    Remove unnecessary IConfigureNamedOptions implementations

--- src/OpenIddict.Validation.AspNetCore/OpenIddictValidationAspNetCoreConfiguration.cs
+++ src/OpenIddict.Validation.AspNetCore/OpenIddictValidationAspNetCoreConfiguration.cs
@@ -5,7 +5,6 @@
  */
 
 using System;
-using System.Diagnostics;
 using System.Text;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Authentication;
@@ -17,7 +16,7 @@ namespace OpenIddict.Validation.AspNetCore
     /// Contains the methods required to ensure that the OpenIddict validation configuration is valid.
     /// </summary>
     public class OpenIddictValidationAspNetCoreConfiguration : IConfigureOptions<AuthenticationOptions>,
-                                                               IConfigureNamedOptions<OpenIddictValidationOptions>,
+                                                               IConfigureOptions<OpenIddictValidationOptions>,
                                                                IPostConfigureOptions<AuthenticationOptions>
     {
         /// <summary>
@@ -46,9 +45,6 @@ namespace OpenIddict.Validation.AspNetCore
         }
 
         public void Configure([NotNull] OpenIddictValidationOptions options)
-            => Debug.Fail("This infrastructure method shouldn't be called");
-
-        public void Configure([CanBeNull] string name, [NotNull] OpenIddictValidationOptions options)
         {
             if (options == null)
             {
