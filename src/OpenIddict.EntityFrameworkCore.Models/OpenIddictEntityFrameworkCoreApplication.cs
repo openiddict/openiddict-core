b2338541d43566c04f7bc5f924182b66399c78cc
Kévin Chalet <kevinchalet@gmail.com>
2020-07-26 17:48:32 +0200

    Add nullable annotations to OpenIddict.EntityFrameworkCore.Models

--- src/OpenIddict.EntityFrameworkCore.Models/OpenIddictEntityFrameworkCoreApplication.cs
+++ src/OpenIddict.EntityFrameworkCore.Models/OpenIddictEntityFrameworkCoreApplication.cs
@@ -7,6 +7,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace OpenIddict.EntityFrameworkCore.Models
 {
@@ -34,7 +35,10 @@ namespace OpenIddict.EntityFrameworkCore.Models
     /// Represents an OpenIddict application.
     /// </summary>
     [DebuggerDisplay("Id = {Id.ToString(),nq} ; ClientId = {ClientId,nq} ; Type = {Type,nq}")]
-    public class OpenIddictEntityFrameworkCoreApplication<TKey, TAuthorization, TToken> where TKey : IEquatable<TKey>
+    public class OpenIddictEntityFrameworkCoreApplication<TKey, TAuthorization, TToken>
+        where TKey : IEquatable<TKey>
+        where TAuthorization : class
+        where TToken : class
     {
         /// <summary>
         /// Gets the list of the authorizations associated with this application.
@@ -42,77 +46,74 @@ namespace OpenIddict.EntityFrameworkCore.Models
         public virtual ICollection<TAuthorization> Authorizations { get; } = new HashSet<TAuthorization>();
 
         /// <summary>
-        /// Gets or sets the client identifier
-        /// associated with the current application.
+        /// Gets or sets the client identifier associated with the current application.
         /// </summary>
-        public virtual string ClientId { get; set; }
+        public virtual string? ClientId { get; set; }
 
         /// <summary>
         /// Gets or sets the client secret associated with the current application.
         /// Note: depending on the application manager used to create this instance,
         /// this property may be hashed or encrypted for security reasons.
         /// </summary>
-        public virtual string ClientSecret { get; set; }
+        public virtual string? ClientSecret { get; set; }
 
         /// <summary>
         /// Gets or sets the concurrency token.
         /// </summary>
-        public virtual string ConcurrencyToken { get; set; } = Guid.NewGuid().ToString();
+        public virtual string? ConcurrencyToken { get; set; } = Guid.NewGuid().ToString();
 
         /// <summary>
-        /// Gets or sets the consent type
-        /// associated with the current application.
+        /// Gets or sets the consent type associated with the current application.
         /// </summary>
-        public virtual string ConsentType { get; set; }
+        public virtual string? ConsentType { get; set; }
 
         /// <summary>
-        /// Gets or sets the display name
-        /// associated with the current application.
+        /// Gets or sets the display name associated with the current application.
         /// </summary>
-        public virtual string DisplayName { get; set; }
+        public virtual string? DisplayName { get; set; }
 
         /// <summary>
         /// Gets or sets the localized display names
         /// associated with the current application,
         /// serialized as a JSON object.
         /// </summary>
-        public virtual string DisplayNames { get; set; }
+        public virtual string? DisplayNames { get; set; }
 
         /// <summary>
-        /// Gets or sets the unique identifier
-        /// associated with the current application.
+        /// Gets or sets the unique identifier associated with the current application.
         /// </summary>
-        public virtual TKey Id { get; set; }
+        [AllowNull, MaybeNull]
+        public virtual TKey Id { get; set; } = default!;
 
         /// <summary>
         /// Gets or sets the permissions associated with the
         /// current application, serialized as a JSON array.
         /// </summary>
-        public virtual string Permissions { get; set; }
+        public virtual string? Permissions { get; set; }
 
         /// <summary>
         /// Gets or sets the logout callback URLs associated with
         /// the current application, serialized as a JSON array.
         /// </summary>
-        public virtual string PostLogoutRedirectUris { get; set; }
+        public virtual string? PostLogoutRedirectUris { get; set; }
 
         /// <summary>
         /// Gets or sets the additional properties serialized as a JSON object,
         /// or <c>null</c> if no bag was associated with the current application.
         /// </summary>
-        public virtual string Properties { get; set; }
+        public virtual string? Properties { get; set; }
 
         /// <summary>
         /// Gets or sets the callback URLs associated with the
         /// current application, serialized as a JSON array.
         /// </summary>
-        public virtual string RedirectUris { get; set; }
+        public virtual string? RedirectUris { get; set; }
 
         /// <summary>
         /// Gets or sets the requirements associated with the
         /// current application, serialized as a JSON array.
         /// </summary>
-        public virtual string Requirements { get; set; }
+        public virtual string? Requirements { get; set; }
 
         /// <summary>
         /// Gets the list of the tokens associated with this application.
@@ -120,9 +121,8 @@ namespace OpenIddict.EntityFrameworkCore.Models
         public virtual ICollection<TToken> Tokens { get; } = new HashSet<TToken>();
 
         /// <summary>
-        /// Gets or sets the application type
-        /// associated with the current application.
+        /// Gets or sets the application type associated with the current application.
         /// </summary>
-        public virtual string Type { get; set; }
+        public virtual string? Type { get; set; }
     }
 }
