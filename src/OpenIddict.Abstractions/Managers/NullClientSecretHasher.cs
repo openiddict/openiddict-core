07c9b2614b563c490ee6088ae1e7a2db5a9036b9
Sebastian <sebastian@squidex.io>
2021-05-16 11:06:14 +0200

    Add null implementation.

---
+++ src/OpenIddict.Abstractions/Managers/NullClientSecretHasher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenIddict.Abstractions
+{
+    /// <summary>
+    /// Implements the <see cref="IOpenIddictClientSecretHasher"/> with a null implementation tht does not hash the secret.
+    /// </summary>
+    /// <remarks>
+    /// Only use this class for custom implementations of the <see cref="IOpenIddictApplicationStore{TApplication}"/> interface when your secret is already hashed.
+    /// </remarks>
+    public class NullClientSecretHasher : IOpenIddictClientSecretHasher
+    {
+        /// <inheritdoc />
+        public ValueTask<string> ObfuscateClientSecretAsync(string secret, CancellationToken cancellationToken = default)
+        {
+            return new ValueTask<string>(secret);
+        }
+
+        /// <inheritdoc />
+        public ValueTask<bool> ValidateClientSecretAsync(string secret, string comparand, CancellationToken cancellationToken = default)
+        {
+            var equals = string.Equals(secret, comparand);
+
+            return new ValueTask<bool>(equals);
+        }
+    }
+}
