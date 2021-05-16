5063b33887a457f073c6d07198816c71fd105dc7
Sebastian <sebastian@squidex.io>
2021-05-16 12:10:54 +0200

    added license information.

--- src/OpenIddict.Abstractions/Managers/NullClientSecretHasher.cs
+++ src/OpenIddict.Abstractions/Managers/NullClientSecretHasher.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/openiddict/openiddict-core for more information concerning
+ * the license and the contributors participating to this project.
+ */
+
 using System.Threading;
 using System.Threading.Tasks;
 
