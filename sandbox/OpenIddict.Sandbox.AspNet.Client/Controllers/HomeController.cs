f063cf56e23950bd98ffdbd8c1376dab53c68051
Kévin Chalet <kevinchalet@gmail.com>
2022-11-27 06:26:01 +0100

    Update the Google integration to support the access_type parameter

--- sandbox/OpenIddict.Sandbox.AspNet.Client/Controllers/HomeController.cs
+++ sandbox/OpenIddict.Sandbox.AspNet.Client/Controllers/HomeController.cs
@@ -1,10 +1,14 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
+using OpenIddict.Client;
 using static OpenIddict.Client.Owin.OpenIddictClientOwinConstants;
 
 namespace OpenIddict.Sandbox.AspNet.Client.Controllers
@@ -12,14 +16,20 @@ namespace OpenIddict.Sandbox.AspNet.Client.Controllers
     public class HomeController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly OpenIddictClientService _service;
 
-        public HomeController(IHttpClientFactory httpClientFactory)
-            => _httpClientFactory = httpClientFactory;
+        public HomeController(
+            IHttpClientFactory httpClientFactory,
+            OpenIddictClientService service)
+        {
+            _httpClientFactory = httpClientFactory;
+            _service = service;
+        }
 
         [HttpGet, Route("~/")]
         public ActionResult Index() => View();
 
-        [Authorize, HttpPost, Route("~/")]
+        [Authorize, HttpPost, Route("~/message")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Index(CancellationToken cancellationToken)
         {
@@ -37,6 +47,40 @@ namespace OpenIddict.Sandbox.AspNet.Client.Controllers
             response.EnsureSuccessStatusCode();
 
             return View(model: await response.Content.ReadAsStringAsync());
+        }
+
+        [Authorize, HttpPost, Route("~/refresh-token")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> RefreshToken(CancellationToken cancellationToken)
+        {
+            var context = HttpContext.GetOwinContext();
+
+            var result = await context.Authentication.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationType);
+            if (!result.Properties.Dictionary.TryGetValue(Tokens.RefreshToken, out string token))
+            {
+                return new HttpStatusCodeResult(400);
+            }
+
+            var (response, principal) = await _service.AuthenticateWithRefreshTokenAsync(
+                issuer: new Uri(result.Identity.Claims.Select(claim => claim.Issuer).First(), UriKind.Absolute),
+                token: token,
+                cancellationToken: cancellationToken);
+
+            var properties = new AuthenticationProperties(result.Properties.Dictionary)
+            {
+                RedirectUri = null
+            };
+
+            properties.Dictionary[Tokens.BackchannelAccessToken] = response.AccessToken;
+
+            if (!string.IsNullOrEmpty(response.RefreshToken))
+            {
+                properties.Dictionary[Tokens.RefreshToken] = response.RefreshToken;
+            }
+
+            context.Authentication.SignIn(properties, result.Identity);
+
+            return View("Index", model: response.AccessToken);
         }
     }
 }
--- sandbox/OpenIddict.Sandbox.AspNetCore.Client/Controllers/HomeController.cs
+++ sandbox/OpenIddict.Sandbox.AspNetCore.Client/Controllers/HomeController.cs
@@ -3,25 +3,31 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using OpenIddict.Client.AspNetCore;
+using OpenIddict.Client;
+using static OpenIddict.Client.AspNetCore.OpenIddictClientAspNetCoreConstants;
 
 namespace OpenIddict.Sandbox.AspNetCore.Client.Controllers;
 
 public class HomeController : Controller
 {
     private readonly IHttpClientFactory _httpClientFactory;
+    private readonly OpenIddictClientService _service;
 
-    public HomeController(IHttpClientFactory httpClientFactory)
-        => _httpClientFactory = httpClientFactory;
+    public HomeController(
+        IHttpClientFactory httpClientFactory,
+        OpenIddictClientService service)
+    {
+        _httpClientFactory = httpClientFactory;
+        _service = service;
+    }
 
     [HttpGet("~/")]
     public ActionResult Index() => View();
 
-    [Authorize, HttpPost("~/"), ValidateAntiForgeryToken]
-    public async Task<ActionResult> Index(CancellationToken cancellationToken)
+    [Authorize, HttpPost("~/message"), ValidateAntiForgeryToken]
+    public async Task<ActionResult> GetMessage(CancellationToken cancellationToken)
     {
-        var token = await HttpContext.GetTokenAsync(CookieAuthenticationDefaults.AuthenticationScheme,
-            OpenIddictClientAspNetCoreConstants.Tokens.BackchannelAccessToken);
+        var token = await HttpContext.GetTokenAsync(CookieAuthenticationDefaults.AuthenticationScheme, Tokens.BackchannelAccessToken);
 
         using var client = _httpClientFactory.CreateClient();
 
@@ -31,6 +37,38 @@ public class HomeController : Controller
         using var response = await client.SendAsync(request, cancellationToken);
         response.EnsureSuccessStatusCode();
 
-        return View(model: await response.Content.ReadAsStringAsync(cancellationToken));
+        return View("Index", model: await response.Content.ReadAsStringAsync(cancellationToken));
+    }
+
+    [Authorize, HttpPost("~/refresh-token"), ValidateAntiForgeryToken]
+    public async Task<ActionResult> RefreshToken(CancellationToken cancellationToken)
+    {
+        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        var token = result?.Properties.GetTokenValue(Tokens.RefreshToken);
+        if (string.IsNullOrEmpty(token))
+        {
+            return BadRequest();
+        }
+
+        var (response, principal) = await _service.AuthenticateWithRefreshTokenAsync(
+            issuer: new Uri(result.Principal.Claims.Select(claim => claim.Issuer).First(), UriKind.Absolute),
+            token: token,
+            cancellationToken: cancellationToken);
+
+        var properties = new AuthenticationProperties(result.Properties.Items)
+        {
+            RedirectUri = null
+        };
+
+        properties.UpdateTokenValue(Tokens.BackchannelAccessToken, response.AccessToken);
+
+        if (!string.IsNullOrEmpty(response.RefreshToken))
+        {
+            properties.UpdateTokenValue(Tokens.RefreshToken, response.RefreshToken);
+        }
+
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, result.Principal, properties);
+
+        return View("Index", model: response.AccessToken);
     }
 }
