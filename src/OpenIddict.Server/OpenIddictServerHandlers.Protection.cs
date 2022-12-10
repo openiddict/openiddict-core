d8a4451603ce2b5b48422c6ee658ef930ad1d41d
Kévin Chalet <kevinchalet@gmail.com>
2022-12-10 10:45:29 +0100

    Support OpenIddictClientOptions.ClientUri/OpenIddictServerOptions.Issuer/OpenIddictValidationOptions.Issuer URIs created with DangerousDisablePathAndQueryCanonicalization=true

--- src/OpenIddict.Server/OpenIddictServerHandlers.Protection.cs
+++ src/OpenIddict.Server/OpenIddictServerHandlers.Protection.cs
@@ -73,13 +73,23 @@ public static partial class OpenIddictServerHandlers
                 {
                     null => null,
 
-                    // If the issuer URI doesn't contain any path/query/fragment, allow both http://www.fabrikam.com
+                    // If the issuer URI doesn't contain any query/fragment, allow both http://www.fabrikam.com
                     // and http://www.fabrikam.com/ (the recommended URI representation) to be considered valid.
                     // See https://datatracker.ietf.org/doc/html/rfc3986#section-6.2.3 for more information.
                     { AbsolutePath: "/", Query.Length: 0, Fragment.Length: 0 } uri => new[]
                     {
                         uri.AbsoluteUri, // Uri.AbsoluteUri is normalized and always contains a trailing slash.
                         uri.AbsoluteUri[..^1]
+                    },
+
+                    // When properly normalized, Uri.AbsolutePath should never be empty and should at least
+                    // contain a leading slash. While dangerous, System.Uri now offers a way to create a URI
+                    // instance without applying the default canonicalization logic. To support such URIs,
+                    // a special case is added here to add back the missing trailing slash when necessary.
+                    { AbsolutePath.Length: 0, Query.Length: 0, Fragment.Length: 0 } uri => new[]
+                    {
+                        uri.AbsoluteUri,
+                        uri.AbsoluteUri + "/"
                     },
 
                     Uri uri => new[] { uri.AbsoluteUri }
