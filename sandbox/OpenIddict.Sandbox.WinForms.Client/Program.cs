fb78fb9d0df271145e1981df274e7246afeaadeb
Kévin Chalet <kevinchalet@gmail.com>
2023-06-17 23:24:51 +0200

    Remove the Add[Provider]() overload returning a derived builder

--- sandbox/OpenIddict.Sandbox.WinForms.Client/Program.cs
+++ sandbox/OpenIddict.Sandbox.WinForms.Client/Program.cs
@@ -81,11 +81,13 @@ var host = new HostBuilder()
                 // parameter containing their URL as part of authorization responses. For more information,
                 // see https://datatracker.ietf.org/doc/html/draft-ietf-oauth-security-topics#section-4.4.
                 options.UseWebProviders()
-                       .AddGitHub()
-                       .SetClientId("cf8efb4d76c0cb7109d3")
-                       .SetClientSecret("e8c0f6b869164411bb9052e42414cbcc52d518cd")
-                       // Note: GitHub doesn't support the recommended ":/" syntax and requires using "://".
-                       .SetRedirectUri("com.openiddict.sandbox.winforms.client://callback/login/github");
+                       .AddGitHub(options =>
+                       {
+                           options.SetClientId("cf8efb4d76c0cb7109d3")
+                                  .SetClientSecret("e8c0f6b869164411bb9052e42414cbcc52d518cd")
+                                  // Note: GitHub doesn't support the recommended ":/" syntax and requires using "://".
+                                  .SetRedirectUri("com.openiddict.sandbox.winforms.client://callback/login/github");
+                       });
             });
 
         // Register the worker responsible for creating the database used to store tokens
