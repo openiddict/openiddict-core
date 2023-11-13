3a952a65a11e34a277e3431aeada7ab196c89a62
Dennis Haney <davh@voxmeter.dk>
2023-11-13 14:03:55 +0700

    feat: add constants to match https://openid.net/specs/openid-connect-core-1_0.html#ClaimsParameter

--- src/OpenIddict.Abstractions/OpenIddictConstants.cs
+++ src/OpenIddict.Abstractions/OpenIddictConstants.cs
@@ -365,6 +365,15 @@ public static class OpenIddictConstants
         public const string VerificationUriComplete = "verification_uri_complete";
     }
 
+    public static class ClaimsParameters
+    {
+       public const string UserInfo = "userinfo";
+       public const string IdToken = "id_token";
+       public const string Essential = "essential";
+       public const string Value = "value";
+       public const string Values = "values";
+    }
+
     public static class Permissions
     {
         public static class Endpoints
