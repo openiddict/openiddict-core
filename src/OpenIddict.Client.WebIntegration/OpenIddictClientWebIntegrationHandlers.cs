5b60cde92dcc55a8d3af0435687955272ba3986a
Mike Alhayek <mike@crestapps.com>
2025-05-08 20:47:02 +0200

    Add Genesys to the list of supported providers

--- src/OpenIddict.Client.WebIntegration/OpenIddictClientWebIntegrationHandlers.cs
+++ src/OpenIddict.Client.WebIntegration/OpenIddictClientWebIntegrationHandlers.cs
@@ -1512,17 +1512,17 @@ public static partial class OpenIddictClientWebIntegrationHandlers
                 ProviderTypes.Atlassian => (string?) context.UserInfoResponse?["account_id"],
 
                 // These providers return the user identifier as a custom "id" node:
-                ProviderTypes.Airtable      or ProviderTypes.Basecamp   or ProviderTypes.Box       or
-                ProviderTypes.Dailymotion   or ProviderTypes.Deezer     or ProviderTypes.Discord   or
-                ProviderTypes.Disqus        or ProviderTypes.Facebook   or ProviderTypes.Gitee     or
-                ProviderTypes.GitHub        or ProviderTypes.Harvest    or ProviderTypes.Kook      or
-                ProviderTypes.Kroger        or ProviderTypes.Lichess    or ProviderTypes.Linear    or 
-                ProviderTypes.Mastodon      or ProviderTypes.Meetup     or ProviderTypes.Miro      or 
-                ProviderTypes.Nextcloud     or ProviderTypes.Patreon    or ProviderTypes.Pipedrive or 
-                ProviderTypes.Reddit        or ProviderTypes.Smartsheet or ProviderTypes.Spotify   or 
-                ProviderTypes.SubscribeStar or ProviderTypes.Todoist    or ProviderTypes.Twitter   or
-                ProviderTypes.Webflow       or ProviderTypes.Weibo      or ProviderTypes.Yandex    or 
-                ProviderTypes.Zoom
+                ProviderTypes.Airtable    or ProviderTypes.Basecamp      or ProviderTypes.Box        or
+                ProviderTypes.Dailymotion or ProviderTypes.Deezer        or ProviderTypes.Discord    or
+                ProviderTypes.Disqus      or ProviderTypes.Facebook      or ProviderTypes.Genesys    or
+                ProviderTypes.Gitee       or ProviderTypes.GitHub        or ProviderTypes.Harvest    or
+                ProviderTypes.Kook        or ProviderTypes.Kroger        or ProviderTypes.Lichess    or
+                ProviderTypes.Linear      or ProviderTypes.Mastodon      or ProviderTypes.Meetup     or
+                ProviderTypes.Miro        or ProviderTypes.Nextcloud     or ProviderTypes.Patreon    or
+                ProviderTypes.Pipedrive   or ProviderTypes.Reddit        or ProviderTypes.Smartsheet or
+                ProviderTypes.Spotify     or ProviderTypes.SubscribeStar or ProviderTypes.Todoist    or
+                ProviderTypes.Twitter     or ProviderTypes.Webflow       or ProviderTypes.Weibo      or
+                ProviderTypes.Yandex      or ProviderTypes.Zoom
                     => (string?) context.UserInfoResponse?["id"],
 
                 // Bitbucket returns the user identifier as a custom "uuid" node:
