55cd0e94a8034d08270f7edbb8febc1295c712aa
Kévin Chalet <kevinchalet@gmail.com>
2025-06-09 23:32:06 +0200

    Update all the token type validation delegates to assume that tokens that don't have a "typ" header are generic JSON Web Tokens

--- src/OpenIddict.Client/OpenIddictClientOptions.cs
+++ src/OpenIddict.Client/OpenIddictClientOptions.cs
@@ -113,9 +113,15 @@ public sealed class OpenIddictClientOptions
         ClockSkew = TimeSpan.Zero,
         NameClaimType = Claims.Name,
         RoleClaimType = Claims.Role,
-        // Note: unlike IdentityModel, this custom validator deliberately uses case-insensitive comparisons.
         TypeValidator = static (type, token, parameters) =>
         {
+            // Assume that tokens that don't have an explicit "typ" header attached are generic JSON Web Tokens.
+            if (string.IsNullOrEmpty(type))
+            {
+                type = JsonWebTokenTypes.GenericJsonWebToken;
+            }
+
+            // Note: unlike IdentityModel, this custom validator deliberately uses case-insensitive comparisons.
             if (parameters.ValidTypes is not null && parameters.ValidTypes.Any() &&
                !parameters.ValidTypes.Contains(type, StringComparer.OrdinalIgnoreCase))
             {
