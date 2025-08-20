392ab99fa31a29a03b814c63691a0f34c4f32fea
Gehongyan <gehongyan1996@126.com>
2025-08-20 19:42:38 +0800

    HeyBoxChat user info API does not require token header
    Signed-off-by: Gehongyan <gehongyan1996@126.com>

--- src/OpenIddict.Client.WebIntegration/OpenIddictClientWebIntegrationHandlers.Userinfo.cs
+++ src/OpenIddict.Client.WebIntegration/OpenIddictClientWebIntegrationHandlers.Userinfo.cs
@@ -124,14 +124,6 @@ public static partial class OpenIddictClientWebIntegrationHandlers
                     request.Headers.Add("X-API-Key", settings.ApplicationKey);
                 }
 
-                // HeyBoxChat requires a "token" header containing the Bot token.
-                else if (context.Registration.ProviderType is ProviderTypes.HeyBoxChat)
-                {
-                    var settings = context.Registration.GetHeyBoxChatSettings();
-
-                    request.Headers.Add("token", settings.Token);
-                }
-
                 // Notion requires sending an explicit API version (which is statically set
                 // to the last version known to be supported by the OpenIddict integration).
                 else if (context.Registration.ProviderType is ProviderTypes.Notion)
