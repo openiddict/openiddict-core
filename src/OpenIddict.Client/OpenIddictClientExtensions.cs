e046ae29fd811919071c11c04c304cd7597e3cbd
Kévin Chalet <kevinchalet@gmail.com>
2026-03-12 18:28:40 +0100

    Introduce a new dynamic option to disable issuer parameter validation and disable it for Google

--- src/OpenIddict.Client/OpenIddictClientExtensions.cs
+++ src/OpenIddict.Client/OpenIddictClientExtensions.cs
@@ -50,6 +50,7 @@ public static class OpenIddictClientExtensions
         builder.Services.TryAddSingleton<RequireIntrospectionClientAssertionGenerated>();
         builder.Services.TryAddSingleton<RequireIntrospectionRequest>();
         builder.Services.TryAddSingleton<RequireIssuedTokenValidated>();
+        builder.Services.TryAddSingleton<RequireIssuerParameterValidationEnabled>();
         builder.Services.TryAddSingleton<RequireLoginStateTokenGenerated>();
         builder.Services.TryAddSingleton<RequireLogoutStateTokenGenerated>();
         builder.Services.TryAddSingleton<RequireJsonWebTokenFormat>();
