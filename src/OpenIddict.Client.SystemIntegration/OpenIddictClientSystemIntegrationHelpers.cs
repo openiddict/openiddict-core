95d9157033a389198df74f0ae06ab51aab533597
Kévin Chalet <kevinchalet@gmail.com>
2026-07-24 15:49:59 +0200

    Attach an IASWebAuthenticationPresentationContextProviding object on Mac Catalyst and macOS

--- src/OpenIddict.Client.SystemIntegration/OpenIddictClientSystemIntegrationHelpers.cs
+++ src/OpenIddict.Client.SystemIntegration/OpenIddictClientSystemIntegrationHelpers.cs
@@ -249,12 +249,10 @@ public static class OpenIddictClientSystemIntegrationHelpers
             {
                 try
                 {
-#pragma warning disable CA1416
                     using var scenes = UIApplication.SharedApplication.ConnectedScenes;
                     var scene = scenes.ToArray<UIWindowScene>().FirstOrDefault();
 
                     return scene?.Windows.FirstOrDefault();
-#pragma warning restore CA1416
                 }
 
                 catch (InvalidCastException)
@@ -272,12 +270,10 @@ public static class OpenIddictClientSystemIntegrationHelpers
             {
                 try
                 {
-#pragma warning disable CA1416
                     using var scenes = UIApplication.SharedApplication.ConnectedScenes;
                     var scene = scenes.ToArray<UIWindowScene>().FirstOrDefault();
 
                     return scene?.Windows;
-#pragma warning restore CA1416
                 }
 
                 catch (InvalidCastException)
