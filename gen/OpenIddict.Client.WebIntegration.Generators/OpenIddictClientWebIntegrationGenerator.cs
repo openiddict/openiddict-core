b74aa66e5789bea356c6619fb56747d835bc8653
Kévin Chalet <kevinchalet@gmail.com>
2026-08-22 11:30:05 +0200

    Update the providers generator to support the AuthorizationResponseIssParameterSupported attribute and use it for the GitHub provider

--- gen/OpenIddict.Client.WebIntegration.Generators/OpenIddictClientWebIntegrationGenerator.cs
+++ gen/OpenIddict.Client.WebIntegration.Generators/OpenIddictClientWebIntegrationGenerator.cs
@@ -1056,6 +1056,8 @@ public sealed partial class OpenIddictClientWebIntegrationConfiguration
                     UserInfoEndpoint = CreateUri($""{{ environment.configuration.user_info_endpoint | string.replace '\'' '""' }}"", UriKind.Absolute),
                     {{~ end ~}}
 
+                    AuthorizationResponseIssParameterSupported = {{ environment.configuration.authorization_response_iss_parameter_supported ?? ""null"" }},
+
                     CodeChallengeMethodsSupported =
                     {
                         {{~ for method in environment.configuration.code_challenge_methods_supported ~}}
@@ -1341,6 +1343,8 @@ public sealed partial class OpenIddictClientWebIntegrationConfiguration
                                     RevocationEndpoint = (string?) configuration.Attribute("RevocationEndpoint"),
                                     TokenEndpoint = (string?) configuration.Attribute("TokenEndpoint"),
                                     UserInfoEndpoint = (string?) configuration.Attribute("UserInfoEndpoint"),
+
+                                    AuthorizationResponseIssParameterSupported = (bool?) configuration.Attribute("AuthorizationResponseIssParameterSupported"),
 
                                     CodeChallengeMethodsSupported = configuration.Elements("CodeChallengeMethod").ToList() switch
                                     {
